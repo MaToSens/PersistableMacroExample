ed2179d6c075238b3bde6cb42e684338873e156c
MaToSens <matoosens@gmail.com>
2025-03-23 08:33:34 +0100

    Added initial files

---
+++ PersistableMacroExample/PersistableMacroExample/PersistableMacroExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistableMacroExampleApp.swift
+//  PersistableMacroExample
+//
+//  Created by MaTooSens on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PersistableMacroExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
